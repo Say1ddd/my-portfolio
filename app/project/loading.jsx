259696a2ee50298c4d182730e8ae1f59777abcc3
Say1ddd <sajidmuhammadikhlas@gmail.com>
2024-01-29 23:26:37 +0700

    Modify: root page

---
+++ app/project/loading.jsx
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function Loading() {
+    return (
+        <div className={styles.loading__container}>
+            <p className={styles.text}>Sajid/Portfolio</p>
+        </div>
+    );
+};
---
+++ app/project/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import { Suspense } from "react";
+import Loading from "./loading";
+
+export default function Test() {
+  return (
+      <div>
+        <p>sajid</p>
+      </div>
+  );
+}
